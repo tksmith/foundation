0b5fa6d26ef189f824f71ae2f1b3e9d0c25c9fc5
Kyle Smith <tksmith@crimson.ua.edu>
2011-12-21 23:15:28 -0600

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+  def hello
+  end
+
+  def goodbye
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foundation::Application.config.secret_token = '78c8934e90b06765ef2a4ddf8b67cca2aedf38e70e94bcfcb12703c991a8b554d877d767d3178b98bc9649186e2e6d2f54b1c1059f0f998949ae97ce437ead2b'
